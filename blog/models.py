1abfaae250ea424ae2557d8061e8a9548f19762c
AnoodALhjaj <Anood8Alhjaj@gmail.com>
2021-01-13 14:17:19 +0300

    compelet Home Page

--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,6 @@ from django.db import models
 # Create your models here.
 class Blog(models.Model):
     title=models.CharField(max_length=255)
-    pub_date=models.DateTimeField(auto_now_add=True)
+    pub_date=models.DateTimeField()
     body=models.TextField()
     images=models.ImageField(default="default.png",blank=True,upload_to='blogs')
