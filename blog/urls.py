5ea46c63b6cf87ad2601952ba631b90f999b7a7b
AnoodALhjaj <Anood8Alhjaj@gmail.com>
2021-01-14 00:07:56 +0300

    compelet the protofilo

--- blog/urls.py
+++ blog/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='index'),
+    path('', views.home, name='blogs'),
     path('<int:blog_id>/', views.details, name='details'),
 
 ]
