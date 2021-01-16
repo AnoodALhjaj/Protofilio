dd834ce13d1ee2f8d89b84a19f6e69e80b56de77
Anood Alhjaj <59451825+AnoodALhjaj@users.noreply.github.com>
2021-01-16 11:58:03 +0300

    update database to be sqlite

--- Protofilio/settings.py
+++ Protofilio/settings.py
@@ -74,15 +74,17 @@ WSGI_APPLICATION = 'Protofilio.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
-
+  #'ENGINE': 'django.db.backends.postgresql',
+   #     'NAME':'protofilo',
+    #    'USER':"postgres",
+     #   "PASSWORD":"",
+      #  "HOST":"localhost",
+       # "PORT":"5432",
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME':'protofilo',
-        'USER':"postgres",
-        "PASSWORD":"Anood$$&&++",
-        "HOST":"localhost",
-        "PORT":"5432",
+         'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+      
     }
 }
 
